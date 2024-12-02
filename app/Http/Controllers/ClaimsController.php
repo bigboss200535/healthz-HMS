97d78f458d6626ca0d020bf9ce0490801e0a66f0
WebEdge Technologies <alhassan.mohammedga@gmail.com>
2024-12-02 21:07:50 +0000

    Claims Controller update

--- app/Http/Controllers/ClaimsController.php
+++ app/Http/Controllers/ClaimsController.php
@@ -6,5 +6,130 @@ use Illuminate\Http\Request;
 
 class ClaimsController extends Controller
 {
-    //
+  
+    public function generate_vetting_claims()
+    {
+        $lock = Cache::lock('generate_claims', 360);
+            if($lock->get())
+            {
+                try {
+                    Log::info('Generating Claims');
+                    $data = $this->gather_date();
+                    $process_data = $this->process_data($data);
+                    $report = $this->create_report($process_data);
+                    $this->sendReport($report);
+                    Log::info('Claims generated successfully');
+                } catch (\Exception $e) {
+                    Log::error('Error generated claims: ' . $e->getMessage());
+                } finally{
+                    $lock->release();
+                   
+                }
+            }else{
+                 Log::info('Claims generation in progress');
+            }
+    }  
+
+    public function xml_claims_generation()
+    {
+        $claims = Claim::with('patient', 'consultation', 'prescriptions')->get();
+        $xml = new SimpleXMLElement('<claims/>');
+
+            foreach ($claims as $claim) {
+                $claimXml = $xml->addChild('claim');
+
+                // Add core claim information
+                $claimXml->addChild('claimID', $claim->claim_id);
+                $claimXml->addChild('membership_no', $claim->membership_no);
+                // ... other core claim fields
+
+                // Add patient demographic information
+                $patientXml = $claimXml->addChild('patient');
+                $patientXml->addChild('surname', $claim->patient->surname);
+                $patientXml->addChild('otherNames', $claim->patient->other_names);
+                // ... other patient fields
+
+                // Add consultation information
+                $consultationXml = $claimXml->addChild('consultation');
+                $consultationXml->addChild('diagnosis', $claim->consultation->diagnosis);
+                $consultationXml->addChild('principalGDRG', $claim->consultation->principal_gdrg);
+
+                // Add prescription information
+                foreach ($claim->prescriptions as $prescription) {
+                    $medicineXml = $claimXml->addChild('medicine');
+                    $medicineXml->addChild('medicineCode', $prescription->medicine_code);
+                    $medicineXml->addChild('dispensedQty', $prescription->dispensed_qty);
+                    // ... other prescription fields
+                }
+            }
+
+            return $xml->asXML();
+            }
+
+            public function json_claims_generation()
+            {
+                $claims = Claim::with('patient', 'consultation', 'prescriptions')->get();
+
+                    $jsonData = [];
+
+                    foreach ($claims as $claim) {
+                        $claimData = [
+                            'claim_id' => $claim->claim_id,
+                            'membership_no' => $claim->membership_no,
+                            // ... other core claim fields
+                            'patient' => [
+                                'surname' => $claim->patient->surname,
+                                'other_names' => $claim->patient->other_names,
+                                // ... other patient fields
+                            ],
+                            'consultation' => [
+                                'diagnosis' => $claim->consultation->diagnosis,
+                                'principal_gdrg' => $claim->consultation->principal_gdrg,
+                            ],
+                            'prescriptions' => [],
+                        ];
+
+                        foreach ($claim->prescriptions as $prescription) {
+                            $claimData['prescriptions'][] = [
+                                'medicine_code' => $prescription->medicine_code,
+                                'dispensed_qty' => $prescription->dispensed_qty,
+                                // ... other prescription fields
+                            ];
+                        }
+
+                        $jsonData[] = $claimData;
+                    }
+
+                    return response()->json($jsonData);
+            }
+            public function index()
+            {
+        
+            }
+        
+            public function create()
+            {
+        
+            }
+        
+            public function edit()
+            {
+        
+            }
+        
+            public function store()
+            {
+        
+            }
+        
+            public function show()
+            {
+        
+            }
+        
+            public function destroy()
+            {
+        
+            }
+
 }
